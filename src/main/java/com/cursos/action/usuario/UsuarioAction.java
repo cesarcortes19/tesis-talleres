cdf83e84987721995f00a632137a18688a0152f0
cesarcortes19 <caracas2>
2014-08-24 21:04:39 -0430

    Usuario Action

---
+++ src/main/java/com/cursos/action/usuario/UsuarioAction.java
@@ -0,0 +1,93 @@
+package com.cursos.action.usuario;
+
+import com.cursos.model.AlumnoModel;
+import com.cursos.model.AlumnoModel;
+import com.cursos.model.UserModel;
+import com.cursos.service.usuario.UsuarioService;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.PredicateUtils;
+
+import java.lang.reflect.Type;
+import java.util.*;
+
+/**
+ * Created by Cesar on 12/08/2014.
+ */
+public class UsuarioAction extends ActionSupport {
+
+    private UserModel usuarioModel;
+    private UsuarioService usuarioService;
+    private String listaAlumnoJson;
+
+    public String execute(){
+        return SUCCESS;
+    }
+
+    public String cargar(){
+        return SUCCESS;
+    }
+
+    public String crear(){
+        Gson gson = new Gson();
+        Type listType = new TypeToken<ArrayList<AlumnoModel>>() {
+        }.getType();
+
+        List<AlumnoModel> alumnoModelSet;
+        alumnoModelSet = (List<AlumnoModel>) CollectionUtils.select(gson.<Collection>fromJson(listaAlumnoJson, listType),
+                PredicateUtils.notNullPredicate());
+        for (AlumnoModel am: alumnoModelSet){
+            am.setUserModel(usuarioModel);
+        }
+          /*alumnoModelSet.iterator().next().setUserModel(usuarioModel);*/
+        Set<AlumnoModel> set = new HashSet<AlumnoModel>(alumnoModelSet);
+
+        usuarioModel.setAlumnoModelSet(set);
+
+        try{
+            usuarioService.guardar(usuarioModel);
+        }catch (Exception e){
+            return ERROR;
+        }
+
+        return "crearUsuario";
+    }
+
+    public String editar(){
+        return SUCCESS;
+    }
+
+    public String editarContrasena(){
+        return SUCCESS;
+    }
+
+    public String eliminar(){
+        return SUCCESS ;
+    }
+
+    public UserModel getUsuarioModel() {
+        return usuarioModel;
+    }
+
+    public void setUsuarioModel(UserModel usuarioModel) {
+        this.usuarioModel = usuarioModel;
+    }
+
+    public String getListaAlumnoJson() {
+        return listaAlumnoJson;
+    }
+
+    public void setListaAlumnoJson(String listaAlumnoJson) {
+        this.listaAlumnoJson = listaAlumnoJson;
+    }
+
+    public void setUsuarioService(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    public UsuarioService getUsuarioService() {
+        return usuarioService;
+    }
+}
